8aada546b9e6c1826aa0acb761d83f74d6907904
Ahmad Al Musawi <almusawiaf@vcu.edui>
2019-08-08 20:50:26 -0400

    ng2 version

--- FileUploader/server.js
+++ FileUploader/server.js
@@ -1,52 +1,108 @@
-const app = require("./Backend/app");
-const debug = require("debug")("node-angular");
-const http = require("http");
+// const app = require("./Backend/app");
+// const debug = require("debug")("node-angular");
+// const http = require("http");
 
-const normalizePort = val => {
-  var port = parseInt(val, 10);
+// const normalizePort = val => {
+//   var port = parseInt(val, 10);
 
-  if (isNaN(port)) {
-    // named pipe
-    return val;
+//   if (isNaN(port)) {
+//     // named pipe
+//     return val;
+//   }
+
+//   if (port >= 0) {
+//     // port number
+//     return port;
+//   }
+
+//   return false;
+// };
+
+// const onError = error => {
+//   if (error.syscall !== "listen") {
+//     throw error;
+//   }
+//   const bind = typeof port === "string" ? "pipe " + port : "port " + port;
+//   switch (error.code) {
+//     case "EACCES":
+//       console.error(bind + " requires elevated privileges");
+//       process.exit(1);
+//       break;
+//     case "EADDRINUSE":
+//       console.error(bind + " is already in use");
+//       process.exit(1);
+//       break;
+//     default:
+//       throw error;
+//   }
+// };
+
+// const onListening = () => {
+//   const addr = server.address();
+//   const bind = typeof port === "string" ? "pipe " + port : "port " + port;
+//   debug("Listening on " + bind);
+// };
+
+// const port = normalizePort(process.env.PORT || "3000");
+// app.set("port", port);
+
+// const server = http.createServer(app);
+// server.on("error", onError);
+// server.on("listening", onListening);
+// server.listen(port);
+
+
+// Version Two
+
+
+/*eslint-disable*/
+var express = require('express');
+var multer = require('multer');
+var fs = require('fs');
+var app = express();
+
+var DIR = './uploads/';
+
+var upload = multer({ dest: DIR });
+
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://valor-software.github.io');
+  res.setHeader('Access-Control-Allow-Methods', 'POST');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
+app.use(multer({
+  dest: DIR,
+  rename: function (fieldname, filename) {
+    return filename + Date.now();
+  },
+  onFileUploadStart: function (file) {
+    console.log(file.originalname + ' is starting ...');
+  },
+  onFileUploadComplete: function (file) {
+    console.log(file.fieldname + ' uploaded to  ' + file.path);
   }
+}));
 
-  if (port >= 0) {
-    // port number
-    return port;
-  }
+app.get('/api', function (req, res) {
+  res.end('file catcher example');
+});
 
-  return false;
-};
+app.post('/api', function (req, res) {
+  upload(req, res, function (err) {
+    if (err) {
+      return res.end(err.toString());
+    }
 
-const onError = error => {
-  if (error.syscall !== "listen") {
-    throw error;
-  }
-  const bind = typeof port === "string" ? "pipe " + port : "port " + port;
-  switch (error.code) {
-    case "EACCES":
-      console.error(bind + " requires elevated privileges");
-      process.exit(1);
-      break;
-    case "EADDRINUSE":
-      console.error(bind + " is already in use");
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-};
+    res.end('File is uploaded');
+  });
+});
 
-const onListening = () => {
-  const addr = server.address();
-  const bind = typeof port === "string" ? "pipe " + port : "port " + port;
-  debug("Listening on " + bind);
-};
+var PORT = process.env.PORT || 3000;
 
-const port = normalizePort(process.env.PORT || "3000");
-app.set("port", port);
+app.listen(PORT, function () {
+  console.log('Working on port ' + PORT);
+});
 
-const server = http.createServer(app);
-server.on("error", onError);
-server.on("listening", onListening);
-server.listen(port);
--- FileUploader/src/app/app.component.ts
+++ FileUploader/src/app/app.component.ts
@@ -1,29 +1,119 @@
-import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+// import { Component } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   selectedFile: File = null;
+
+//   constructor(private http: HttpClient){
+
+//   }
+//   onFileSelected(event){
+//     this.selectedFile = <File>event.target.files[0];
+//     console.log(event)
+//   }
+
+//   onUpload(){
+//     const fd = new FormData();
+//     fd.append('image', this.selectedFile, this.selectedFile.name);
+//     console.log ("this is the data on the frontend", fd);
+//     this.http.post('http://localhost:8000/upload', fd)
+//       .subscribe(res =>{
+//         console.log ("this is the file sent", res)
+//       });
+//   }
+// }
+
+// version two
+// import { Component } from '@angular/core';
+// import { FileUploader } from 'ng2-file-upload';
+ 
+// // const URL = '/api/';
+// const URL = 'https://evening-anchorage-3159.herokuapp.com/api/';
+ 
+// @Component({
+//   selector: 'simple-demo',
+//   templateUrl: './simple-demo.html'
+// })
+// export class SimpleDemoComponent {
+//   public uploader:FileUploader = new FileUploader({url: URL});
+//   public hasBaseDropZoneOver:boolean = false;
+//   public hasAnotherDropZoneOver:boolean = false;
+ 
+//   public fileOverBase(e:any):void {
+//     this.hasBaseDropZoneOver = e;
+//   }
+ 
+//   public fileOverAnother(e:any):void {
+//     this.hasAnotherDropZoneOver = e;
+//   }
+// }
+
+import { Component, EventEmitter } from '@angular/core';
+import { FileUploader, FileLikeObject } from 'ng2-file-upload';
+ 
+function readBase64(file): Promise<any> {
+    var reader  = new FileReader();
+    var future = new Promise((resolve, reject) => {
+      reader.addEventListener("load", function () {
+        resolve(reader.result);
+      }, false);
+
+      reader.addEventListener("error", function (event) {
+        reject(event);
+      }, false);
+
+      reader.readAsDataURL(file);
+    });
+    return future;
+  }
+
+// const URL = '/api/';
+const URL = 'https://evening-anchorage-3159.herokuapp.com/api/';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'my-app',
+  styles: [`
+      .my-drop-zone { border: dotted 3px lightgray; }
+    .nv-file-over { border: dotted 3px red; } 
+    .another-file-over-class { border: dotted 3px green; }    
+ 
+    html, body { height: 100%; }
+  `],
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  selectedFile: File = null;
+  public uploader:FileUploader = new FileUploader({
+    url: URL, 
+    disableMultipart:true
+    });
+  public hasBaseDropZoneOver:boolean = false;
+  public hasAnotherDropZoneOver:boolean = false;
 
-  constructor(private http: HttpClient){
+  fileObject: any;
 
+
+  public fileOverBase(e:any):void {
+    this.hasBaseDropZoneOver = e; 
   }
-  onFileSelected(event){
-    this.selectedFile = <File>event.target.files[0];
-    console.log(event)
+ 
+  public fileOverAnother(e:any):void {
+    this.hasAnotherDropZoneOver = e;
   }
 
-  onUpload(){
-    const fd = new FormData();
-    fd.append('image', this.selectedFile, this.selectedFile.name);
-    console.log ("this is the data on the frontend", fd);
-    this.http.post('http://localhost:8000/upload', fd)
-      .subscribe(res =>{
-        console.log ("this is the file sent", res)
-      });
+  public onFileSelected(event: EventEmitter<File[]>) {
+    const file: File = event[0];
+
+    console.log(file);
+
+    readBase64(file)
+      .then(function(data) {
+      console.log(data);
+    })
+
   }
 }
